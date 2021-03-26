a69ac6bf4eb3d06741335e1bf4ce3dac72af4f10
Ross Wardrup <minorsecond@gmail.com>
2021-03-26 04:53:47 -0600

    Fix node table border

--- main.js
+++ main.js
@@ -477,7 +477,7 @@ map.on('singleclick', function (evt) {
             document.getElementById('info').innerHTML =
                 "<table class=\"styled-table\">\n" +
                 "    <thead>\n" +
-                "      <tr><th colspan='4' class='table-title'>Node</th></tr>" +
+                "      <tr><th colspan='3' class='table-title'>Node</th></tr>" +
                 "        <tr>\n" +
                 "            <th>Call</th>\n" +
                 "            <th>Grid</th>\n" +
